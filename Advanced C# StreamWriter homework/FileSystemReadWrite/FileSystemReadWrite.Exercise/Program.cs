36755490f91e0ce7013e5888d69d0d5d14b4c92f
Dragisha-Todoroski <120037342+Dragisha-Todoroski@users.noreply.github.com>
2023-04-28 18:13:34 +0200

    Add files via upload

---
+++ Advanced C# StreamWriter homework/FileSystemReadWrite/FileSystemReadWrite.Exercise/Program.cs
@@ -0,0 +1,44 @@
+﻿string relativePath = @"..\..\..\";
+string exerciseFolder = relativePath + @"Exercise\";
+string calculationsFile = exerciseFolder + "calculations.txt";
+
+if (!Directory.Exists(exerciseFolder))
+{
+    Directory.CreateDirectory(exerciseFolder);
+    Console.ForegroundColor = ConsoleColor.Green;
+    Console.WriteLine("Sucessfully created Exercise folder");
+    Console.ResetColor();
+}
+
+if (!File.Exists(calculationsFile))
+{
+    File.Create(calculationsFile).Close();
+    Console.ForegroundColor = ConsoleColor.Yellow;
+    Console.WriteLine("Sucessfully created Calculations file");
+    Console.ResetColor();
+}
+
+string Calculate(int num1, int num2)
+{
+    return $"{num1} + {num2} = {num1 + num2}";
+}
+
+Console.WriteLine("Enter first number:");
+string firstNum = Console.ReadLine();
+bool firstNumCheck = int.TryParse(firstNum, out int parsedFirstNum);
+Console.WriteLine("Enter second number:");
+string secondNum = Console.ReadLine();
+bool secondNumCheck = int.TryParse(secondNum, out int parsedSecondNum);
+
+if (firstNumCheck && secondNumCheck)
+{
+    Console.WriteLine(Calculate(parsedSecondNum, parsedFirstNum));
+    using (StreamWriter sw = new StreamWriter(calculationsFile, true))
+    {
+        sw.WriteLine($"{Calculate(parsedFirstNum, parsedSecondNum)} - {DateTime.Now.ToLocalTime()}");
+    }
+}
+else
+{
+    Console.WriteLine("Wrong inputs");
+}
